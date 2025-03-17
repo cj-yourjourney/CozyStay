e28ea8daabb7919f7e9ad60b794a5c60e911b5e1
cj <49462194+cj-yourjourney@users.noreply.github.com>
2025-03-17 14:07:22 -0700

    created properties app and list view

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,11 +38,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "corsheaders",
-
-
     ### Third-party apps
     "rest_framework",
     "profiles",
+    "properties",
 ]
 
 MIDDLEWARE = [
---
+++ backend/properties/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Property
+from .serializers import PropertySerializer
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+# Create your views here.
+
+
+@api_view(['GET'])
+def property_list(request):
+    properties = Property.objects.all()
+    serializer = PropertySerializer(properties, many=True)
+
+    return Response(serializer.data)
